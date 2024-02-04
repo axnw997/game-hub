0baaccc46e80f65d57af19b1588919cf4243f5d6
Ellie Wang <axnw997@aucklanduni.ac.nz>
2024-02-03 18:06:14 -0800

    build game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,23 @@
+import { SimpleGrid } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { error, games } = useGame();
   return (
     <div>
       {error && <p>{error}</p>}
-      <ul>
+      <SimpleGrid columns={{
+        base:1,
+        sm:1,
+        md:2,
+        lg:3,
+        xl:5
+      }} spacing={10} padding={10}>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </div>
   );
 };
