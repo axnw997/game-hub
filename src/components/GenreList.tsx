fc230110c6c48eef400abaa8529b17cc2046b350
Ellie Wang <axnw997@aucklanduni.ac.nz>
2024-02-16 13:45:56 +1300

    cleaning up code

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -15,35 +15,40 @@ interface Props {
   selectedGenre: Genres | null;
 }
 
-const GenreList = ({ onSelectGenre,selectedGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      <Heading>Genres</Heading>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-              boxSize="32px"
-            ></Image>
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              fontSize="lg"
-              variant="link"
-              colorScheme= {genre.id === selectedGenre?.id ? "green" : ""}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <div><Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
+      <List>
+        
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                objectFit={"cover"}
+              ></Image>
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+                colorScheme={genre.id === selectedGenre?.id ? "green" : ""}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </div>
   );
 };
 
